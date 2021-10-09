62db30399abc230b1c1b77cdb2aab1c881782cbe
Andrea055 <vivanco11.ac@gmail.com>
2021-10-09 19:11:32 +0200

    Add casting operation to add value

--- main.py
+++ main.py
@@ -11,44 +11,52 @@ client = commands.Bot(command_prefix='.')
 async def nanopoolergo(ctx, *, arg):
     nanopoolwork = requests.get('https://api.nanopool.org/v1/ergo/balance/' + arg)
     nanopool_data = nanopoolwork.json()
-    await ctx.send(nanopool_data['data'])
+    ergobalance = str(nanopool_data['data'])
+    await ctx.send(ergobalance + " ERG")
 
 
 @client.command()
 async def ergowallet(ctx, *, arg):
     ergoexp = requests.get('https://api.ergoplatform.com/api/v1/addresses/' + arg + '/balance/total')
     egoexpdate = ergoexp.json()
-    await ctx.send(egoexpdate['confirmed']['nanoErgs'])
+    ergobalance = str(egoexpdate['confirmed']['nanoErgs'])
+    await ctx.send(ergobalance + " ERG")
+
 
 @client.command()
 async def flypool(ctx, *, arg):
     flypoolapi = requests.get('https://api-ergo.flypool.org/miner/" + arg + "/dashboard')
     flyerg = flypoolapi.json()
-    await ctx.send(flyerg['data']['currentStatistics']['unpaid'] / 1000000000)
+    ergobalance = str(flyerg['data']['currentStatistics']['unpaid'] / 1000000000)
+    await ctx.send(ergobalance + " ERG")
 
 @client.command()
 async def nanopooleth(ctx, *, arg):
     nanopooleth = requests.get('https://api.nanopool.org/v1/eth/balance/' + arg)
     nanopoolapi = nanopooleth.json()
-    await ctx.send("Pool bilance" + nanopoolapi['data'])
+    ethbalance = str(nanopoolapi['data'])
+    await ctx.send(ethbalance + " ETH")
 
 @client.command()
 async def twominerseth(ctx, *, arg):
     nanopooleth = requests.get('https://eth.2miners.com/api/accounts/' + arg)
     nanopoolapi = nanopooleth.json()
-    await ctx.send("Pool bilance" + nanopoolapi['stats']['paid'] / 1000000000)
+    ethbalance = str(nanopoolapi['stats']['paid'] / 1000000000)
+    await ctx.send(ethbalance + " ETH")
 
 @client.command()
 async def ethermine(ctx, *, arg):
     nanopooleth = requests.get('https://api.ethermine.org/miner/' + arg + "/dashboard")
     nanopoolapi = nanopooleth.json()
-    await ctx.send("Pool bilance" + nanopoolapi['data']['currentStatistics']['unpaid'] / 1000000000000000000)
+    ethbalance = str(nanopoolapi['data']['currentStatistics']['unpaid'] / 1000000000000000000)
+    await ctx.send(ethbalance + " ETH")
 
 @client.command()
 async def hiveon(ctx, *, arg):
     hiveeth = requests.get('https://hiveon.net/api/v1/stats/miner/walletAddress123/' + wallet + "ETH/billing-acc")
     hiveapi = hiveeth.json()
     bilance = (hiveapi['succeedPayouts'] + hiveapi['pendingPayouts']) / 1000000000000000000
-    await ctx.send("Pool bilance" + bilance)
+    ethbalance = str(bilance)
+    await ctx.send(ethbalance + " ETH")
 
 client.run('TOKEN')
